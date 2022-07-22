35d08fbf1454741f3ceedfc845bae66cfbc970bd
George Williams <48786161+flying-rock@users.noreply.github.com>
2022-07-22 16:16:49 +0800

    Add files via upload

---
+++ install_required_packages.R
@@ -0,0 +1,11 @@
+# Script to help people install all the required packages
+# that are used in this analysis - assuming they don't already have them all
+# May 2022
+
+# install all packages - this may take a couple minutes
+install.packages('tidyverse',
+                 'readxl',
+                 'tm',
+                 'maps',
+                 'plotly',
+                 'zoo')
